600d22997e9d6973e112470ddd0633d1aa18af47
Rojemmarc Gutierrez <gutierrezrojemmarc4@gmail.com>
2026-03-01 17:49:50 +0800

    Codes for classes vehicle, motorbike, and car.

--- ParkingLotManagementSystem/src/vehicles/Car.java
+++ ParkingLotManagementSystem/src/vehicles/Car.java
@@ -1,4 +1,25 @@
 package vehicles;
 
-public class Car {
+public class Car extends Vehicle {
+    String carBrand;
+    String carModel;
+    String transmissionType;
+
+    public Car(String ownerName, String plateNumber, int entryTime, int exitTime,
+               int numberOfDoors, String fuelType, String transmissionType, String carModel) {
+        super(ownerName, "Car", plateNumber, entryTime, exitTime);
+        this.carBrand = carModel;
+        this.carModel = carModel;
+        this.transmissionType = transmissionType;
+    }
+
+    public String getCarBrand() { return carBrand; }
+    public String getCarModel() { return carModel; }
+    public String getTransmissionType() { return transmissionType; }
+
+    @Override
+    public String toString() {
+        return "Car [" + getPlateNumber() + "] owned by " + getOwnerName() + " | Model: " + carModel + ", Doors: "
+                + ", Transmission: " + transmissionType;
+    }
 }
--- ParkingLotManagementSystem/src/vehicles/Motorbike.java
+++ ParkingLotManagementSystem/src/vehicles/Motorbike.java
@@ -1,4 +1,22 @@
 package vehicles;
 
-public class Motorbike {
+public class Motorbike extends Vehicle {
+    private int engineCapacity;
+    private boolean hasSidecar;
+    private String motorbikeModel;
+
+    public Motorbike(String ownerName, String plateNumber, int entryTime,
+                     int engineCapacity, boolean hasSidecar, String motorbikeModel) {
+        super(ownerName, plateNumber, entryTime);
+        this.engineCapacity = engineCapacity;
+        this.hasSidecar = hasSidecar;
+        this.motorbikeModel = motorbikeModel;
+    }
+
+    @Override
+    public String toString() {
+        return toString() + " | Model: " + motorbikeModel +
+                ", Engine: " + engineCapacity + "cc, Sidecar: " + (hasSidecar ? "Yes" : "No");
+    }
 }
+
